d52b75f85f8a519834d68fffbd9e06c4643c12b8
Thomas Markiewicz <tom@pumpitbetter.com>
2025-03-22 14:54:22 -0500

    Added weight unit dialog that opens on setting press

--- app/routes/app.settings/route.tsx
+++ app/routes/app.settings/route.tsx
@@ -5,6 +5,7 @@ import { MainContent } from "~/components/MainContent";
 import { Page } from "~/components/Page";
 import { List } from "~/components/List";
 import { ListItem } from "~/components/ListItem";
+import { DialogWeightUnit } from "~/components/Dialog/dialog-weight-unit";
 
 export async function clientLoader() {
   const settings = await db.settings.findOne().exec();
@@ -18,11 +19,13 @@ export default function Settings({ loaderData }: Route.ComponentProps) {
       <Header title="Settings" />
       <MainContent>
         <List>
-          <ListItem
-            title="Weight Unit"
-            content={weigthUnit}
-            onClick={() => console.log("ListItem clicked")}
-          />
+          <DialogWeightUnit
+            onOpenChange={(open) => {
+              if (!open) console.log("Dialog closed, save changes here");
+            }}
+          >
+            <ListItem title="Weight Unit" content={weigthUnit} />
+          </DialogWeightUnit>
         </List>
       </MainContent>
     </Page>
