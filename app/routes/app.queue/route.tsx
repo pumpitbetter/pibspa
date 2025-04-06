f3516cf291881a892d13d09538097db50ae36be0
Thomas Markiewicz <tom@pumpitbetter.com>
2025-04-06 10:45:43 -0500

    Updated queue screen title to program name

--- app/routes/app.queue/route.tsx
+++ app/routes/app.queue/route.tsx
@@ -1,11 +1,41 @@
 import { Header } from "~/components/Header";
 import { MainContent } from "~/components/MainContent";
 import { Page } from "~/components/Page";
+import { db } from "~/db/db";
+import { defaultProgram } from "~/db/programs";
+import type { Route } from "./+types/route";
 
-export default function Queue() {
+export async function clientLoader() {
+  const settings = await db.settings.findOne().exec();
+  const program = await db.programs
+    .findOne({
+      selector: {
+        id: settings?.programId,
+      },
+    })
+    .exec();
+  const workouts = await db.workouts
+    .find({
+      selector: {
+        programId: settings?.programId,
+      },
+      sort: [{ order: "asc" }],
+    })
+    .exec();
+  const exercises = await db.exercises.find().exec();
+
+  return {
+    program: program ? program.toMutableJSON() : defaultProgram,
+    workouts: workouts ? workouts.map((w) => w.toMutableJSON()) : [],
+    exercises: exercises ? exercises.map((e) => e.toMutableJSON()) : [],
+  };
+}
+
+export default function Queue({ loaderData }: Route.ComponentProps) {
+  const { program, workouts, exercises } = loaderData;
   return (
     <Page>
-      <Header title="Workout Queue" />
+      <Header title={program.name} />
       <MainContent>This is a queue of upcoming program workouts.</MainContent>
     </Page>
   );
