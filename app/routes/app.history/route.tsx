7825707733ed4ed74ca344380016bcb1a1ec3ae6
Thomas Markiewicz <tom@pumpitbetter.com>
2025-05-07 19:45:14 -0500

    Added calendar to history screen

--- app/routes/app.history/route.tsx
+++ app/routes/app.history/route.tsx
@@ -1,12 +1,20 @@
 import { Header } from "~/components/header";
 import { MainContent } from "~/components/main-content";
 import { Page } from "~/components/page";
+import { Calendar } from "~/components/ui/calendar";
 
 export default function History() {
   return (
     <Page>
       <Header title="History" />
-      <MainContent>Main content here...</MainContent>
+      <MainContent>
+        <Calendar
+          classNames={{
+            head_row: "flex justify-between",
+            row: "flex w-full justify-between mt-2 min-h-[36px]",
+          }}
+        />
+      </MainContent>
     </Page>
   );
 }
