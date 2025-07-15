855c64413de2f90c7647a92a199d98cbda825423
Chris Markiewicz <50533471+Chris-Markiewicz@users.noreply.github.com>
2025-07-14 18:38:48 -0700

    feat: Add ownerId to programs to make them read-only (#18)
    Closes #6

--- app/db/programs-five31-trident.ts
+++ app/db/programs-five31-trident.ts
@@ -3,6 +3,7 @@ import type { ProgramsDocType } from "./programs";
 export const five31Trident: ProgramsDocType = {
   id: "531-trident",
   name: "5/3/1 - Trident",
+  ownerId: "system",
   description:
     "An intermediate strength program with progression increasing weight of compound lifts every 4 week cycle.  One big lift plus two complementary accessory lifts per session",
   type: "strength",
